ebb257ef74e1f976be38a6b68ce615631650fe20
nKadykov <nkadykov@gmail.com>
2024-06-10 14:30:36 +0300

    add: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,16 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello world" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Online pong");
+    boost::asio::io_context io_context;
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
     return 0;
 }
